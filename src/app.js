66f2a7d5075356f639b7c817e1794eb8e5500927
Alex Turtu <turtu.alexandru@gmail.com>
2022-01-22 10:53:15 +0200

    Nav bar ready
    added CSS
    created cards for testing

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import { showDetails } from "./details.js";
+showDetails();
+import { Product } from "./Product.js";
+var product1 = new Product(1, "iphoe", 1999, "nNew Iphone");
+console.log(product1);
---
+++ src/details.js
@@ -0,0 +1,4 @@
+export function showDetails() {
+  console.log("Details");
+}
+export class Details {}
