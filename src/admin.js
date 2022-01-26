9461ef35fd8c54ba507cb972453dbbd5bd0a7c67
Alex Turtu <turtu.alexandru@gmail.com>
2022-01-26 12:01:13 +0200

    Changed the way cards are generated
    added remove fucntion and add fucntion to remove or add API

--- src/admin.js
+++ src/admin.js
@@ -1,38 +1,130 @@
-var adminContainer = document.querySelector(".adminContainer");
-var editButton;
-var newAdminCard;
-var removeButon;
+const adminContainer = document.querySelector(".adminContainer");
+const adminCard = document.querySelector(".adminCard");
+let editButton = document.querySelector(".editProductButton");
+let removeButon = document.querySelector(".removeProductButton");
+const editMenu = document.querySelector(".editproduct");
+const addNewProduct = document.getElementById("addNewProduct");
+const closeEditBtn = document.getElementById("closeEdit");
+let newAdminCard;
+// edit or add variables
+const newProductTitle = document.getElementById("newProductTitle");
+const newShortDescription = document.getElementById("newShortDescription");
+const newProductPrice = document.getElementById("newProductPrice");
+const newProductImage = document.getElementById("newProductImage");
+const saveButton = document.getElementById("saveEdit");
+let idToDelte;
 
 fetch("https://61ed969d634f2f00170cec8b.mockapi.io/perimetruProducts")
   .then((result) => result.json())
   .then((data) => {
-    for (var i = 0; i < data.length; i++) {
+    data.forEach((element) => {
       newAdminCard = document.createElement("div");
-      //picture, title, description, price
+      newAdminCard.id = element.id;
+      // create card (picture, title, short description, price) !except buttons
       newAdminCard.innerHTML = `     
-      <div class="adminImg"><img src="${data[i].img}" alt="Cannot show picture"></div>
-      <div class="adminTitle">${data[i].title}</div>
-      <div class="adminDescription">${data[i].description}</div>
-      <div class="adminPrice"> ${data[i].price} RON</div>`;
+      <div class="adminImg"><img src="${element.img}" alt="Cannot show picture"></div>
+      <div class="adminTitle">${element.title}</div>
+      <div class="adminDescription">${element.description}</div>
+      <div class="adminPrice"> ${element.price} RON</div>`;
+      ///edit button create
       editButton = document.createElement("button");
       editButton.classList.add("buttonProduct");
       editButton.classList.add("editProductButton");
-      editButton.innerHTML = `<i class="fas fa-edit"></i><p>Edit</p>`;
+      editButton.innerHTML = `<i class="fas fa-edit"></i>Edit`;
       newAdminCard.appendChild(editButton);
+      //remove button create
       removeButon = document.createElement("button");
       removeButon.classList.add("buttonProduct");
       removeButon.classList.add("removeProductButton");
-      removeButon.innerHTML = `<i class="fas fa-window-close"></i><p>Remove Product</p>`;
+      removeButon.innerHTML = `<i class="fas fa-window-close removeProductButton"></i>Remove Product`;
       newAdminCard.appendChild(removeButon);
       newAdminCard.classList.add("adminCard");
       adminContainer.appendChild(newAdminCard);
-    }
+      // add event listeners for card buttons
+      editButton.addEventListener("click", showEditMenu);
+      removeButon.addEventListener("click", removeProduct);
+    });
   });
 
-var editButton = document.querySelector(".adminCard");
+closeEditBtn.addEventListener("click", closeEditMenu);
+addNewProduct.addEventListener("click", addNewProductFunction);
+saveButton.addEventListener("click", addProductToAPI);
 
-editButton.addEventListener("click", showEditMenu);
+function showEditMenu(e) {
+  editMenu.classList.remove("editAnimation");
+  editMenu.classList.remove("hidden");
+  editMenu.classList.add("editAnimationOpen");
+  setTimeout(() => {
+    editMenu.classList.remove("editAnimationOpen");
+  }, 700);
+  if ((e.target.id = "addNewProduct")) {
+    document.getElementById("editCardTitle").innerHTML =
+      "Edit existing product";
+    //add fucntions to edit product below
+    console.log(e.target);
+    console.log("edit prod");
+  }
+}
 
-function showEditMenu() {
-  console.log("clicked!");
+function addNewProductFunction(e) {
+  editMenu.classList.remove("editAnimation");
+  editMenu.classList.remove("hidden");
+  editMenu.classList.add("editAnimationOpen");
+  setTimeout(() => {
+    editMenu.classList.remove("editAnimationOpen");
+  }, 700);
+  if ((e.target.id = "addNewProduct")) {
+    document.getElementById("editCardTitle").innerHTML = "Add new product";
+    //add function to add product below
+    console.log(e.target);
+    console.log("add prod");
+  }
+}
+function closeEditMenu() {
+  editMenu.classList.add("editAnimation");
+  setTimeout(() => {
+    editMenu.classList.add("hidden");
+  }, 700);
+}
+
+function removeProduct(event) {
+  if (event.target.classList.contains("removeProductButton")) {
+    event.target.parentNode.remove();
+    // add function to remove data from api below
+    idToDelte = event.target.parentNode.id;
+    console.log(idToDelte);
+    removeFromAPI();
+  }
+}
+
+async function addProductToAPI() {
+  var response = await fetch(
+    `https://61ed969d634f2f00170cec8b.mockapi.io/perimetruProducts`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: `${newProductTitle.value}`,
+        description: `${newShortDescription.value}`,
+        price: newProductPrice.value,
+        img: `${newProductImage.value}`,
+      }),
+    }
+  );
+  setTimeout(() => {
+    closeEditMenu();
+  }, 700);
+}
+async function removeFromAPI() {
+  var response = await fetch(
+    `https://61ed969d634f2f00170cec8b.mockapi.io/perimetruProducts/${idToDelte}`,
+    {
+      method: "delete",
+    }
+  );
+  setTimeout(() => {
+    closeEditMenu();
+  }, 700);
 }
--- src/store.js
+++ src/store.js
@@ -1,39 +1,60 @@
 // fill the cards with data from the MockAPI
+const cardContainer = document.querySelector(".cardContainer");
+let newCard;
+let test1;
+let cardButtons;
+let shortDescription;
+let card;
+let detilasCardDiv;
+let detailsButton;
+let addToCart;
 
-var cardContainer = document.querySelector(".cardContainer");
-var newCard;
 fetch("https://61ed969d634f2f00170cec8b.mockapi.io/perimetruProducts")
   .then((result) => result.json())
   .then((data) => {
     for (var i = 0; i < data.length; i++) {
       newCard = document.createElement("div");
       newCard.id = data[i].id;
-      newCard.innerHTML = `
-      <div  id="${data[i].id}"class="card">
-      <!-- CARD START  -->
-      <div class="img">
-        <img class="cardImg" src="${data[i].img}" alt="No Image">
-      </div>
-      <div class="cardBottom">
-        <div class="shortDescription"> <p id="productTitle">${data[i].title}</p>
-          <p id="productDescription">${data[i].description}</p>
-
-        </div>
-        <div class="cardPrice">Price: ${data[i].price} RON</div>
-        <div class="cardButtons">
-          <div class="detailsBTN">
-            <button class="btn-grad" id="detilsBtnID" onclick="document.location='details.html'"><i id="detilsIcon" class="fas fa-info-circle"></i>Details</button>
-          </div>
-          <div class="addToCart">
-            <button class="btn-grad" id="addToCartID">Add to Cart<i class="fas fa-shopping-cart"></i></button>
-          </div>
-        </div>
-      </div>
-      <!-- CARD END  -->
-
-    <!-- END OF CARD CONTAINER  -->
-  </div>
-      `;
       cardContainer.appendChild(newCard);
+      card = document.createElement("div");
+      card.classList.add("card");
+      card.innerHTML = `<div class="img">
+      <img class="cardImg" src="${data[i].img}" alt="No Image">
+    </div>
+    
+      <div class="shortDescription"> <p id="productTitle">${data[i].title}</p>
+        <p class="productDescription">${data[i].description}</p>
+
+    
+      <div class="cardPrice">Price: ${data[i].price} RON</div>`;
+      newCard.appendChild(card);
+      cardButtons = document.createElement("div");
+      cardButtons.classList.add("cardButtons");
+      card.appendChild(cardButtons);
+      // detilasCardDiv = document.createElement("div");
+      // detilasCardDiv.classList.add("detailsBTN");
+      // cardButtons.appendChild(detilasCardDiv);
+      detailsButton = document.createElement("button");
+      detailsButton.classList.add("btn-grad");
+      detailsButton.classList.add("detilasCardButton");
+      detailsButton.innerHTML = `<i id="detilsIcon" class="fas fa-info-circle"></i>Details`;
+      cardButtons.appendChild(detailsButton);
+      addToCart = document.createElement("button");
+      addToCart.classList.add("btn-grad");
+      addToCart.classList.add("addToCartCardButton");
+      addToCart.innerHTML = `<i class="fas fa-shopping-cart"></i>Add to Cart`;
+      cardButtons.appendChild(addToCart);
+      addToCart.addEventListener("click", addToCartFunction);
+      detailsButton.addEventListener("click", detailsFunction);
     }
   });
+function addToCartFunction(e) {
+  if (e.target.classList.contains("addToCartCardButton")) {
+    console.log(e.target);
+  }
+}
+function detailsFunction(e) {
+  if (e.target.classList.contains("detilasCardButton")) {
+    console.log(e.target);
+  }
+}
